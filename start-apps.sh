3ae6e09fd129fe51c04e79331d47c09e1f68fc8c
daryllmoya <daryllmoya123@gmail.com>
2023-08-06 02:58:44 +0800

    fix: corrected paths of bundled apps

--- start-apps.sh
+++ start-apps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Change directory to the UI app
-cd harmoney-test-ui
+cd test-ui
 yarn start --port=3001 &
 
 # Change directory to the API app
 cd ..
-cd harmoney-test-api
+cd test-api
 yarn start:prod &
