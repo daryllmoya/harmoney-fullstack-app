67bff40d7673eb3c1d481ed92c915850fcd224f7
daryllmoya <daryllmoya123@gmail.com>
2023-08-06 15:11:23 +0800

    feat: attempt fix

--- ui-app/app/api/axios.ts
+++ ui-app/app/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://harmoney-api-app-odnadgi3ua-uc.a.run.app/',
 });
 
 export default api;
